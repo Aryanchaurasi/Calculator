171b70caac5af2fad5c6ccd85df0b81bdd155984
ARYANCHAURASIA <aryanchaurasi20@gmail.com>
2025-01-06 23:48:51 +0530

    Multiplication Function added

--- calculator.java
+++ calculator.java
@@ -12,6 +12,9 @@ public class calculator
 		System.out.println("Addition of Number is :"+ AdditionAnswer);
 		int SubstractionAnswer = Substraction(num1 , num2, num3);
          	System.out.println("Substraction of Number is :"+ SubstractionAnswer);
+		int MultiplicationAnswer = Multiplication(num1 , num2, num3);
+         	System.out.println("Multiplication of Number is :"+ MultiplicationAnswer);
+
 	}
 
 	private static int Addition(int n1 , int n2 , int n3)
@@ -22,5 +25,11 @@ public class calculator
     	{
 		 return n1 - n2 - n3 ;
     	}
+	private static int Multiplication(int n1 , int n2 , int n3)
+    	{
+		 return n1 * n2 * n3 ;
+    	}
+
+
 
 }
